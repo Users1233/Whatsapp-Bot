0aabca1d44d9a5c7163c277c2246190da3328064
Users1233 <85868410+Users1233@users.noreply.github.com>
2021-07-09 12:19:50 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,60 +1,11 @@
-console.log('Starting...')
-let { spawn } = require('child_process')
-let path = require('path')
-let fs = require('fs')
-let package = require('./package.json')
-const CFonts  = require('cfonts')
-CFonts.say('Joseph\nWhatsApp Online Bot', {
-  font: 'chrome',
-  align: 'center',
-  gradient: ['red', 'magenta']
-})
-CFonts.say(`'${package.name}' By @${package.author.name || package.author}`, {
-  font: 'console',
-  align: 'center',
-  gradient: ['red', 'magenta']
-})
-
-var isRunning = false
-/**
- * Start a js file
- * @param {String} file `path/to/file`
- */
-function start(file) {
-  if (isRunning) return
-  isRunning = true
-  let args = [path.join(__dirname, file), ...process.argv.slice(2)]
-  CFonts.say([process.argv[0], ...args].join(' '), {
-    font: 'console',
-    align: 'center',
-    gradient: ['red', 'magenta']
-  })
-  let p = spawn(process.argv[0], args, {
-    stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-  })
-  p.on('message', data => {
-    console.log('[COLLECTED]', data)
-    switch (data) {
-      case 'reset':
-        p.kill()
-        isRunning = false
-        start.apply(this, arguments)
-        break
-      case 'uptime':
-        p.send(process.uptime())
-        break
-    }
-  })
-  p.on('exit', code => {
-    isRunning = false
-    console.error('Exited with code:', code)
-    if (code === 0) return
-    fs.watchFile(args[0], () => {
-      fs.unwatchFile(args[0])
-      start(file)
-    })
-  })
-  // console.log(p)
+window.onload = () => {
+let chat = document.querySelector('div.container-fluid')
+function addMsg(obj) {
+  let html = document.createElement('span')
+  html.className = 'msg'
+  html.innerHTML = obj
+  chat.appendChild(html)
 }
 
-start('main.js')
+window.onclick = () => addMsg(12)
+}
